1b7c6a35be9072b89e2feafab1a857259ec6756d
glitch6663018457 <joaogabrielmartinsdacruz3@gmail.com>
2026-07-11 21:35:44 -0300

    Add secure test script to modify player attributes
    The script modifies the player's humanoid properties for speed and jump power.

--- script.lua
+++ script.lua
@@ -1,1 +1,11 @@
+-- Script de Teste Seguro
+local player = game.Players.LocalPlayer
+if player and player.Character then
+    local humanoid = player.Character:FindFirstChildOfClass("Humanoid")
+    if humanoid then
+        humanoid.WalkSpeed = 50
+        humanoid.JumpPower = 100
+        print("Script ativado com sucesso!")
+    end
+end
 
